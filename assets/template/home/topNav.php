0d5af4fd12ae5912fc33171f77d90c566baa9537
mbrooks8 <mbrooks8@ucmerced.edu>
2018-06-04 11:50:25 -0700

    Renamed hestia to my portfolio

--- assets/template/home/topNav.php
+++ assets/template/home/topNav.php
@@ -1,16 +1,16 @@
 <!DOCTYPE html>
 <html>
     <head>
-        <title>HestiaMS | Home</title>
+        <title>Michael's Portfolio</title>
         <meta http-equiv="Content-Type" content="text/html; charset=UTF-8"/>
         <meta name="viewport" content="width=device-width, initial-scale=1, maximum-scale=1.0"/>
         <link rel="stylesheet" type="text/css"  href="./assets/css/hestia.css" async>
         <link rel="icon" href="../assets/img/logo.png">
         <link rel="manifest" href="./manifest.json">
         <meta name="theme-color"  content="#00CC99">
-        <meta name="description" content="Property Management Software geared towards giving you the ability to efficiently manage your proporties.">
+        <meta name="description" content="Hey! This is my porfolio! Give me a job please.">
         <link rel="icon" href="./assets/img/logo.png">
-        <meta name="author" content="Michael Brooks, Esteban Perez">
+        <meta name="author" content="Michael Brooks">
         <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.min.css" integrity="sha256-eZrrJcwDc/3uDhsdt61sL2oOBY362qM3lon1gyExkL0=" crossorigin="anonymous" defer/>
         <script src="https://code.jquery.com/jquery-3.2.1.min.js" integrity="sha256-hwg4gsxgFZhOsEEamdOYGBf13FyQuiTwlAQgxVSNgt4=" crossorigin="anonymous"></script>
         <script>
@@ -49,9 +49,9 @@
                     <div class="nav-wrapper">
                         <a href="./index" class="logo"><img src="./assets/img/logo.svg" style="height:100%;" alt="Logo"></a>
                         <ul class="left hide-on-med-and-down">
-                            <li><a class="black-text" href="./index.php">Hestia Property management</a></li>
-                            <li><a href="./projects.php">Projects</a></li>
-                            <li><a href="./donate.php">Donate</a></li>
+                            <li><a class="black-text" href="./index.php">Michael's Portfolio</a></li>
+                            <li><a class="black-text" href="./projects.php">Projects</a></li>
+                            <li><a class="black-text" href="./donate.php">Donate</a></li>
                         </ul>
                         <ul class="right">
                             <li><a href="./login" class="black-text">Log In</a></li>
