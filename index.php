0f9e175a546f2aa5300028418d1d81266cf00fe3
mbrooks8 <mbrooks8@ucmerced.edu>
2018-06-08 21:04:46 -0700

    Test commit

--- index.php
+++ index.php
@@ -3,6 +3,14 @@ include("./assets/template/home/topNav.php");
 ?>
 <h1>Hey, I'm Michael</h1>
 <h1>This is my site</h1>
+
+<h2>Projects</h2>
+<p>Here are some of the things I have worked on!</p>
+
+<h3>1</h3>
+<h3>2</h3>
+<h3>3</h3>
+<h3>4</h3>
 <?php
 include("./assets/template/home/botNav.php");
 ?>
